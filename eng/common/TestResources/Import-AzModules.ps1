5964eb4ee9b4c680df297d237b6c8a6e51f3008b
Wes Haggard <Wes.Haggard@microsoft.com>
2021-04-13 15:57:41 -0700

    Testing ACR PS Modules

--- eng/common/TestResources/Import-AzModules.ps1
+++ eng/common/TestResources/Import-AzModules.ps1
@@ -1,11 +1,24 @@
 . "$PSScriptRoot/AzurePowerShellV4/Utility.ps1"
 
-if ($IsWindows) {
-    # Copied from https://github.com/microsoft/azure-pipelines-tasks/blob/9cc8e1b3ee37dc023c81290de1dd522b77faccf7/Tasks/AzurePowerShellV4/AzurePowerShell.ps1#L57-L58
-    CleanUp-PSModulePathForHostedAgent
-    Update-PSModulePathForHostedAgent
+$VerbosePreference = "Continue"
+
+Write-Host "Impoorted module list before:"
+Get-Module
+
+if ($IsWindows)
+{
+  # Copied from https://github.com/microsoft/azure-pipelines-tasks/blob/9cc8e1b3ee37dc023c81290de1dd522b77faccf7/Tasks/AzurePowerShellV4/AzurePowerShell.ps1#L57-L58
+  CleanUp-PSModulePathForHostedAgent
+  Update-PSModulePathForHostedAgent
 }
-else {
-    # Copied from https://github.com/microsoft/azure-pipelines-tasks/blob/9cc8e1b3ee37dc023c81290de1dd522b77faccf7/Tasks/AzurePowerShellV4/InitializeAz.ps1#L16
-    Update-PSModulePathForHostedAgentLinux
+else
+{
+  # Copied from https://github.com/microsoft/azure-pipelines-tasks/blob/9cc8e1b3ee37dc023c81290de1dd522b77faccf7/Tasks/AzurePowerShellV4/InitializeAz.ps1#L16
+  Update-PSModulePathForHostedAgentLinux
 }
+
+Write-Host "Imported module list after:"
+Get-Module
+
+Write-Host "Modules available:"
+Get-Module -ListAvailable
--- sdk/containerregistry/test-resources-pre.ps1
+++ sdk/containerregistry/test-resources-pre.ps1
@@ -5,8 +5,16 @@ param (
   [string] $TestApplicationSecret
 )
 
+#Testing
+Get-Module -ListAvailable
+
+Get-Command -Module "Az.ContainerRegistry"
+
 Import-AzContainerRegistryImage `
   -ResourceGroupName $DeploymentOutputs['CONTAINERREGISTRY_RESOURCE_GROUP'] `
   -RegistryName $DeploymentOutputs['CONTAINERREGISTRY_USERNAME'] `
   -SourceImage 'library/hello-world' -SourceRegistryUri 'registry.hub.docker.com' `
   -Mode 'Force'
+
+Write-Host "Exiting"
+exit 1
