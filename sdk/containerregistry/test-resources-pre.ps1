04b4c787a19dbf0996b51fb0c28da6a3ca80bd8a
Wes Haggard <Wes.Haggard@microsoft.com>
2021-04-13 16:28:44 -0700

    Fix pre script

--- sdk/containerregistry/test-resources-pre.ps1
+++ sdk/containerregistry/test-resources-pre.ps1
@@ -1,10 +1,14 @@
+# Use same parameter names as declared in eng/New-TestResources.ps1 (assume validation therein).
+[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
 param (
-  [string]$ResourceGroupName
+  # Captures any arguments from eng/New-TestResources.ps1 not declared here (no parameter errors).
+  [Parameter(ValueFromRemainingArguments = $true)]
+  $RemainingArguments
 )
 
 $ErrorActionPreference = "Continue"
 
-Import-AzContainerRegistryImage
+Import-AzContainerRegistryImage -?
 
 Write-Host "Exiting"
 exit 1
